e61e0f56c96b00c46195941de012106e51457027
zezoallord <zeyadahmmed20042020@gmail.com>
2023-08-06 07:28:10 -0700

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	void(argc);
 	printf("%s\n", *argv);
 
 	return (0);
